7fe5a8fe33da5490723803537dc4c2fb0995a72f
Oliver Cottrell <olivercottrelln@gmail.com>
2024-01-22 02:31:28 +0000

    updating

---
+++ 0001_ToySecurity/0001_toy_security.py
@@ -0,0 +1,23 @@
+from pygame import mixer 
+import os
+user = os.getlogin()
+user_home = os.path.expanduser(f'~{user}')
+
+mixer.init()
+
+def main():
+    mixer.music.load(f'{user_home}/code/raphael-kit/music/my_music.mp3')
+    mixer.music.set_volume(0.7)
+    mixer.music.play()
+    while True:
+        pass# Dosn't do anything
+
+def destroy():
+    mixer.music.stop()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        destroy()
+
