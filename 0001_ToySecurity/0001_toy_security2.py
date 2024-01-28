b5c9d57a10090b268ee7bfc159497e59a4b6667d
Oliver Cottrell <olivercottrelln@gmail.com>
2024-01-28 06:10:11 +0000

    adding

--- 0001_ToySecurity/0001_toy_security2.py
+++ 0001_ToySecurity/0001_toy_security2.py
@@ -1,5 +1,41 @@
 from gpiozero import DistanceSensor
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import Device, LED
+Device.pin_factory = PiGPIOFactory('127.0.0.1')
+import time
+from time import sleep
+
+
+#-------------------
+# Init
+#-------------------
+
 ultrasonic = DistanceSensor(echo=23, trigger=24, threshold_distance=0.1)
 
-while True: 
-    print("distance =",ultrasonic.distance*1000)
+
+#-------------------
+# Setup
+#-------------------
+
+def setup():
+    pass
+
+
+def destroy():
+    pass
+
+#-------------------
+# Main body
+#-------------------
+def main():
+    while True: 
+        print("distance =",ultrasonic.distance*1000)
+        sleep(0.2)
+        
+
+if __name__ == '__main__':
+    try:
+        setup()
+        main()
+    except KeyboardInterrupt:
+        destroy()
